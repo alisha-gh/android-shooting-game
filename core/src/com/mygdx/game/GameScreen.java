decd6f98a071fb090c6204abbc599e4a0d79dc8c
Alisha <alisha.k3226@gmail.com>
2023-04-16 18:15:35 +0930

    Add Player Texture but not displaying

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,25 +1,37 @@
 package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen implements Screen {
     private OrthographicCamera camera;
-    private SpriteBatch batch;
     private Texture background;
     private MyGdxGame game;
     public enum GameState { PLAYING, COMPLETE }
+    GameState gameState = GameState.PLAYING;
 
     public static final float MOVEMENT_SPEED = 200.0f;
     public static final float GOAL_BOB_HEIGHT = 5.0f;
+
+    //Game clock
+    float dt;
+
+    //Player Character
+    Texture playerTexture;
+    Sprite playerSprite;
+    Vector2 playerDelta;
+    Rectangle playerDeltaRectangle;
+
     //Map and rendering
     SpriteBatch spriteBatch;
     SpriteBatch uiBatch; //Second SpriteBatch without camera transforms, for drawing UI
-    GameState gameState = GameState.PLAYING;
-    BitmapFont bmfont;
 
     //UI textures
     Texture buttonSquareTexture;
@@ -34,14 +46,14 @@ public class GameScreen implements Screen {
     Button moveUpButton;
     Button restartButton;
 
+    boolean restartActive;
+
     // constructor to keep a reference to the main Game class
     public GameScreen(MyGdxGame game) {
         this.game = game;
     }
-
     @Override
     public void show() {
-        Gdx.app.log("GameScreen: ", "gameScreen show called");
         create();
     }
 
@@ -52,9 +64,22 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        batch.begin();
-        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.end();
+        dt = Gdx.graphics.getDeltaTime();
+
+        //Clear the screen every frame before drawing.
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA); //Allows transparent sprites/tiles
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        spriteBatch.setProjectionMatrix(camera.combined);
+        spriteBatch.begin();
+        //playerSprite.setPosition(100,100);
+        playerSprite.setX(100); // Set player sprite's x-coordinate to 0 (left side of the screen)
+        playerSprite.setY(Gdx.graphics.getHeight() / 2 - playerSprite.getHeight() / 2); // Set player sprite's y-coordinate to the middle of the screen
+        playerSprite.draw(spriteBatch);
+        spriteBatch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        spriteBatch.end();
 
         //Draw UI
         uiBatch.begin();
@@ -66,7 +91,7 @@ public class GameScreen implements Screen {
                 moveDownButton.draw(uiBatch);
                 moveUpButton.draw(uiBatch);
 
-              } break;
+            } break;
             //if gameState is Complete: Draw Restart button
             case COMPLETE: {
                 restartButton.draw(uiBatch);
@@ -90,11 +115,11 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-        batch.dispose();
+        spriteBatch.dispose();
         if (background != null) {
             background.dispose();
         }
-        //playerTexture.dispose();
+        playerTexture.dispose();
         buttonSquareTexture.dispose();
         buttonSquareDownTexture.dispose();
         buttonLongTexture.dispose();
@@ -104,23 +129,29 @@ public class GameScreen implements Screen {
     // Helper method to set up the camera, batch, particle effect, and background image
     private void create() {
         Gdx.app.log("GameScreen: ", "gameScreen create");
+        spriteBatch = new SpriteBatch();
+        uiBatch = new SpriteBatch();
+
+        //Camera
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
         float screenRatio = w / h; // Calculate the screen ratio
         camera = new OrthographicCamera();
-        batch = new SpriteBatch();
         background = new Texture(Gdx.files.internal("Backgrounds/07/Repeated.png"));
         camera.setToOrtho(false, background.getHeight() * screenRatio, background.getHeight());
 
-        //Rendering
-        spriteBatch = new SpriteBatch();
-        uiBatch = new SpriteBatch();
-
         //Textures
+        playerTexture = new Texture("Plane02/Moving/skeleton-MovingNIdle_0.png");
         buttonSquareTexture = new Texture("buttons/buttonSquare_blue.png");
         buttonSquareDownTexture = new Texture("buttons/buttonSquare_beige_pressed.png");
         buttonLongTexture = new Texture("buttons/buttonLong_blue.png");
         buttonLongDownTexture = new Texture("buttons/buttonLong_beige_pressed.png");
+
+        //Player
+        playerSprite = new Sprite(playerTexture);
+        playerSprite.setSize(48, 48);
+        playerDelta = new Vector2();
+        playerDeltaRectangle = new Rectangle(0, 0, playerSprite.getWidth(), playerSprite.getHeight());
 
         //Buttons
         float buttonSize = h * 0.1f;
@@ -129,5 +160,22 @@ public class GameScreen implements Screen {
         moveDownButton = new Button(buttonSize, 0.0f, buttonSize, buttonSize, buttonSquareTexture, buttonSquareDownTexture);
         moveUpButton = new Button(buttonSize, buttonSize*2, buttonSize, buttonSize, buttonSquareTexture, buttonSquareDownTexture);
         restartButton = new Button(w/2 - buttonSize*2, h * 0.2f, buttonSize*4, buttonSize, buttonLongTexture, buttonLongDownTexture);
+
+        newGame();
+    }
+
+    private void newGame(){
+        gameState = GameState.PLAYING;
+
+        //Translate camera to center of screen
+        //camera.position.x = 16;
+        //camera.position.y = 16;
+        dt = 0.0f;
+
+        //Player start location
+        playerSprite.setCenter(100,100); //TODO
+        camera.translate(playerSprite.getX(), playerSprite.getY());
+
+        restartActive = false;
     }
 }
